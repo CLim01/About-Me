f4f4db0b28dea77901a23df170383f31d21f3639
임성빈 <95527804+CLim01@users.noreply.github.com>
2022-03-15 09:40:34 +0900

    Add files via upload

---
+++ About Me/About Me/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by 임성빈 on 2022/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "person")
+                }
+
+            StoryView()
+                .tabItem {
+                    Label("Story", systemImage: "book")
+                }
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star")
+                }
+            
+            FunFactsView()
+                .tabItem {
+                    Label("Fun Facts", systemImage: "hand.thumbsup")
+                }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ About Me/About Me/Model/Data.swift
@@ -0,0 +1,38 @@
+//
+//  Data.swift
+//  About Me
+//
+//  Created by 임성빈 on 2022/03/15.
+//
+
+import Foundation
+import SwiftUI
+
+struct Info {
+    let image: String
+    let name: String
+    let story: String
+    let hobbies: [String]
+    let foods: [String]
+    let colors: [Color]
+    let funFacts: [String]
+}
+
+let information = Info(
+    image: "Placeholder",
+    name: "임성빈",
+    story: "SwiftUI를 홀로 공부하고 있는 미래 iOS 개발자 입니다.\n아직은 많이 부족하지만 노력으로 빠르게 성장하겠습니다! ",
+    hobbies: ["bicycle", "ticket.fill", "book.fill"],
+    foods: ["🥐", "🌮", "🍣"],
+    colors: [Color.blue, Color.purple, Color.pink],
+    funFacts: [
+        "The femur is the longest and largest bone in the human body.",
+        "The moon is 238,900 miles away.",
+        "Prince’s last name was Nelson.",
+        "503 new species were discovered in 2020.",
+        "Ice is 9 percent less dense than liquid water.",
+        "You can spell every number up to 1,000 without using the letter A.\n\n...one, two, three, four...ninety-nine...nine hundred ninety-nine 🧐",
+        "A collection of hippos is called a bloat.",
+        "White sand beaches are made of parrotfish poop.",
+    ]
+)
